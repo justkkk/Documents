7366cd9e039b71b1d424c2072abdad60c173d2c8
justkkk <40882974+justkkk@users.noreply.github.com>
2019-01-11 08:47:07 +0000

    Hello

---
+++ hello/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Hello',
+    'version': '1.1',
+    'category': 'Accounting',
+    'description': """
+
+""",
+    'depends': ['account'],
+    'data': ['account_views.xml'],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+}
